ff65629f36b29118ad2451ecd5cb927d7025961c
skrewbar <skurua1244@gmail.com>
2026-09-15 11:33:46 +0900

    ✨ Complete assignment 05P14

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,23 @@
+int ledPin = 7;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  }
+  digitalWrite(ledPin, LOW);
+
+  while (1);
+}
